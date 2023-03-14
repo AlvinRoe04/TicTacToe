0c313cc402967ac7f1f399d95fa860be53824749
Alvin <alvinroeiv@gmail.com>
2023-03-13 20:51:39 -0400

    Bugs caused by moduliarizing code. Need troubleshooting.

--- tictactoe.js
+++ tictactoe.js
@@ -17,17 +17,27 @@ const gameManager = (() => {
 
 const gameBoard = (() => {
     let row = -1;
-    let column = -1;
     let openSlots = 9;
-    const rowReference = {
+    const empty = 0;
+    const rows = {
         upper: 0,
         mid: 1,
         lower: 2
     }
-    const columnReference = {
+    const columns = {
         left: 0,
         center: 1,
         right: 2
+    }
+    const rowStrings = {
+        0: 'upper',
+        1: 'mid',
+        2: 'lower'
+    }
+    const columnStrings = {
+        0: 'left',
+        1: 'center',
+        2: 'right' 
     }    
     const board = [
         [0, 0, 0],
@@ -35,8 +45,6 @@ const gameBoard = (() => {
         [0, 0, 0]
     ];
     const chooseRow = (rowNumber) => row = rowNumber;
-    const chooseColumn = (columnNumber) => column = columnNumber;
-    const get = (row, column) => board[row][column];
     const reset = () => {
         const gameBoardHTML = document.querySelectorAll('.gameSpace');
         for(let row = 0; row < 3; row++){
@@ -47,37 +55,45 @@ const gameBoard = (() => {
         gameBoardHTML.forEach(space => space.innerHTML = '');
         openSlot = 9;
     }
-    const setSpace = (row, column) => {
-
-    }
     const isOutOfSpace = () => openSlots <= 0;
 
     const hasWinner = () => {
-        return (board[rowReference.upper][columnReference.left] !== 0 && 
-            (((board[rowReference.upper][columnReference.left] === board[rowReference.upper][columnReference.mid] && board[rowReference.upper][columnReference.mid] === board[rowReference.upper][columnReference.right]) 
-            || (board[rowReference.upper][columnReference.left] === board[rowReference.mid][columnReference.left] && board[rowReference.mid][columnReference.left] === board[rowReference.lower][columnReference.left]))
-            || (board[rowReference.upper][columnReference.left] === board[rowReference.mid][columnReference.mid] && board[rowReference.mid][columnReference.mid] === board[rowReference.lower][columnReference.right])))
-            || (board[rowReference.mid][columnReference.left] != 0 && (board[rowReference.mid][columnReference.left] === board[rowReference.mid][columnReference.mid] && board[rowReference.mid][columnReference.mid] === board[rowReference.mid][columnReference.right]))
-            || (board[rowReference.lower][columnReference.left] !== 0 && ((board[rowReference.lower][columnReference.left] === board[rowReference.lower][columnReference.mid] && board[rowReference.lower][columnReference.mid] === board[rowReference.lower][columnReference.right])
-            || (board[rowReference.lower][columnReference.left] === board[rowReference.mid][columnReference.mid] && board[rowReference.mid][columnReference.mid] === board[rowReference.upper][columnReference.right])))
-            || (board[rowReference.upper][columnReference.mid] !== 0 && board[rowReference.upper][columnReference.mid] === board[rowReference.mid][columnReference.mid] && board[rowReference.mid][columnReference.mid] === board[rowReference.lower][columnReference.mid])
-            || (board[rowReference.upper][columnReference.right] !== 0 && board[rowReference.upper][columnReference.right] === board[rowReference.mid][columnReference.right] && board[rowReference.mid][columnReference.right] === board[rowReference.lower][columnReference.right]);
+        return (board[rows.upper][columns.left] !== 0 && 
+            (((board[rows.upper][columns.left] === board[rows.upper][columns.mid] && board[rows.upper][columns.mid] === board[rows.upper][columns.right]) 
+            || (board[rows.upper][columns.left] === board[rows.mid][columns.left] && board[rows.mid][columns.left] === board[rows.lower][columns.left]))
+            || (board[rows.upper][columns.left] === board[rows.mid][columns.mid] && board[rows.mid][columns.mid] === board[rows.lower][columns.right])))
+            || (board[rows.mid][columns.left] != 0 && (board[rows.mid][columns.left] === board[rows.mid][columns.mid] && board[rows.mid][columns.mid] === board[rows.mid][columns.right]))
+            || (board[rows.lower][columns.left] !== 0 && ((board[rows.lower][columns.left] === board[rows.lower][columns.mid] && board[rows.lower][columns.mid] === board[rows.lower][columns.right])
+            || (board[rows.lower][columns.left] === board[rows.mid][columns.mid] && board[rows.mid][columns.mid] === board[rows.upper][columns.right])))
+            || (board[rows.upper][columns.mid] !== 0 && board[rows.upper][columns.mid] === board[rows.mid][columns.mid] && board[rows.mid][columns.mid] === board[rows.lower][columns.mid])
+            || (board[rows.upper][columns.right] !== 0 && board[rows.upper][columns.right] === board[rows.mid][columns.right] && board[rows.mid][columns.right] === board[rows.lower][columns.right]);
     }
 
-    return {chooseRow, chooseColumn, get, reset, setSpace, isOutOfSpace, hasWinner, rowReference, columnReference};
+    const onColumnPicked = (column) => {
+        if(gameManager.isGameOver()) {
+            reset();
+            return;
+        }
+        
+        console.log('Row: ' + row);
+        console.log(`Column: ${column}`);
+
+        if(board[row][column] !== empty) return;
+
+        const queryString = `.gameSpace.${rowStrings[row]}.${columnStrings[column]}`;
+        const elementPicked = document.querySelector(queryString);
+        
+        board[row][column] = gameManager.getPlayerNumber();
+        elementPicked.innerHTML = gameManager.getActiveSymbol();
+        gameManager.switchPlayerTurn();
+        if(gameBoard.hasWinner()) {
+            gameManager.setGameOver(true);
+            console.log('Game had winner');
+        }
+    }
+
+    return {chooseRow, onColumnPicked, isOutOfSpace, hasWinner, rows, columns, board};
 })();
-
-const columnStrings = {
-    0: 'left',
-    1: 'mid',
-    2: 'right'
-};
-
-const rowStrings = {
-    0: 'upper',
-    1: 'center',
-    2: 'lower'
-};
 
 const upperSpaces = document.querySelectorAll('.gameSpace.upper');
 const centerSpaces = document.querySelectorAll('.gameSpace.center');
@@ -86,65 +102,12 @@ const leftSpaces = document.querySelectorAll('.gameSpace.left');
 const midSpaces = document.querySelectorAll('.gameSpace.mid');
 const rightSpaces = document.querySelectorAll('.gameSpace.right');
 
-
-let rowPicked = -1;
-let columnPicked = -1; 
-
-upperSpaces.forEach(space => space.addEventListener('click', e => rowPicked = upper));
-centerSpaces.forEach(space => space.addEventListener('click', e => rowPicked = mid));
-lowerSpaces.forEach(space => space.addEventListener('click', e => rowPicked = lower));
-leftSpaces.forEach(space => space.addEventListener('click', e => onColumnPicked(left, gameManager.getPlayerNumber())));
-midSpaces.forEach(space => space.addEventListener('click', e => onColumnPicked(mid, gameManager.getPlayerNumber())));
-rightSpaces.forEach(space => space.addEventListener('click', e => onColumnPicked(right, gameManager.getPlayerNumber())));
-
-
-//Returns boolean if any of the winning conditions are fulfilled.
-function hasWinner() {
-    return (gameBoard[upper][left] !== 0 && 
-        (((gameBoard[upper][left] === gameBoard[upper][mid] && gameBoard[upper][mid] === gameBoard[upper][right]) 
-        || (gameBoard[upper][left] === gameBoard[mid][left] && gameBoard[mid][left] === gameBoard[lower][left]))
-        || (gameBoard[upper][left] === gameBoard[mid][mid] && gameBoard[mid][mid] === gameBoard[lower][right])))
-        || (gameBoard[mid][left] != 0 && (gameBoard[mid][left] === gameBoard[mid][mid] && gameBoard[mid][mid] === gameBoard[mid][right]))
-        || (gameBoard[lower][left] !== 0 && ((gameBoard[lower][left] === gameBoard[lower][mid] && gameBoard[lower][mid] === gameBoard[lower][right])
-        || (gameBoard[lower][left] === gameBoard[mid][mid] && gameBoard[mid][mid] === gameBoard[upper][right])))
-        || (gameBoard[upper][mid] !== 0 && gameBoard[upper][mid] === gameBoard[mid][mid] && gameBoard[mid][mid] === gameBoard[lower][mid])
-        || (gameBoard[upper][right] !== 0 && gameBoard[upper][right] === gameBoard[mid][right] && gameBoard[mid][right] === gameBoard[lower][right]);
-}
-
-function onColumnPicked(columnNumber, placement){
-    if(gameManager.isGameOver()) {
-        resetGame();
-        return;
-    }
-    
-    columnPicked = columnNumber;
-
-    if(gameBoard[rowPicked][columnPicked] !== empty) return;
-
-    const queryString = `.gameSpace.${rowStrings[rowPicked]}.${columnStrings[columnPicked]}`;
-    const elementPicked = document.querySelector(queryString);
-
-    gameBoard[rowPicked][columnPicked] = placement;
-    elementPicked.innerHTML = gameManager.getActiveSymbol();
-    gameManager.switchPlayerTurn();
-    if(hasWinner()){
-        gameManager.setGameOver(true);
-    }    
-}
-
-function resetGame() {
-    const gameBoardHTML = document.querySelectorAll('.gameSpace');
-
-    for(let row = 0; row < 3; row++){
-        for(let column = 0; column < 3; column++){
-            gameBoard[row][column] = empty;
-        }
-    }
-
-    gameBoardHTML.forEach(space => space.innerHTML = '');
-    gameManager.setGameOver(false);
-    gameManager.resetOpenSlotCount();
-}
+upperSpaces.forEach(space => space.addEventListener('click', e => gameBoard.chooseRow(gameBoard.rows.upper)));
+centerSpaces.forEach(space => space.addEventListener('click', e => gameBoard.chooseRow(gameBoard.rows.mid)));
+lowerSpaces.forEach(space => space.addEventListener('click', e => gameBoard.chooseRow(gameBoard.rows.lower)));
+leftSpaces.forEach(space => space.addEventListener('click', e => gameBoard.onColumnPicked(gameBoard.columns.left)));
+midSpaces.forEach(space => space.addEventListener('click', e => gameBoard.onColumnPicked(gameBoard.columns.center)));
+rightSpaces.forEach(space => space.addEventListener('click', e => gameBoard.onColumnPicked(gameBoard.columns.right)));
 
 
 
